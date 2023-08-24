06304a086f061becb6d55e82003fd81dcb9e319c
Qomplainerz <messanyrecordings@yahoo.de>
2023-08-24 21:25:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const searchForm = document.getElementById('search-form');
     const searchInput = document.getElementById('search-input');
     const searchResults = document.getElementById('search-results');
+    
+    // Define a data structure to store search results
+    const searchData = [];
 
-    // Simulated search data (replace with your own data)
-    const searchData = [
-        { title: "Getting Started with QP Search", url: "https://example.com/getting-started", description: "Learn how to use QP Search for effective searches." },
-        { title: "Customizing QP Search Experience", url: "https://example.com/customization", description: "Personalize QP Search to match your preferences." },
-        // Add more search results here
-    ];
-
-    searchForm.addEventListener('submit', function (event) {
+    searchForm.addEventListener('submit', async function (event) {
         event.preventDefault();
         const searchTerm = searchInput.value;
-        
+
         // Clear previous search results
         searchResults.innerHTML = '';
+        searchData.length = 0; // Clear existing search data
 
-        // Filter search data based on the search term
-        const filteredResults = searchData.filter(result => {
-            return result.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                   result.description.toLowerCase().includes(searchTerm.toLowerCase());
-        });
+        // Call the web crawler function with the search term
+        await crawlWebsites(searchTerm);
 
         // Display search results
-        if (filteredResults.length > 0) {
-            filteredResults.forEach((result, index) => {
+        if (searchData.length > 0) {
+            searchData.forEach((result, index) => {
                 const resultItem = document.createElement('div');
                 resultItem.classList.add('result-item');
                 resultItem.innerHTML = `
                     <h3>${index + 1}. ${result.title}</h3>
-                    <p>${result.description}</p>
-                    <a href="${result.url}" target="_blank">Read more</a>
+                    <p>${result.url}</p>
                 `;
                 searchResults.appendChild(resultItem);
             });
@@ -41,4 +34,37 @@ document.addEventListener('DOMContentLoaded', function () {
             searchResults.appendChild(noResults);
         }
     });
+
+    // Function to crawl websites and populate searchData
+    async function crawlWebsites(searchTerm) {
+        // Implement web crawling logic here
+        // Fetch web pages, analyze content, and store relevant results in searchData
+    }
 });
+
+async function crawlWebsites(searchTerm) {
+    // Define a list of seed URLs (websites to start crawling from)
+    const seedUrls = [
+        'https://www.example.com',
+        'https://www.anotherexample.com'
+        // Add more seed URLs as needed
+    ];
+
+    for (const seedUrl of seedUrls) {
+        try {
+            const response = await fetch(seedUrl);
+            const html = await response.text();
+
+            // Use a library like cheerio to parse HTML and extract relevant information
+            const title = 'Extracted Title'; // Replace with actual extracted title
+            const url = seedUrl; // Use the current seed URL
+
+            // Check if the title or content contains the search term
+            if (title.toLowerCase().includes(searchTerm.toLowerCase()) || html.toLowerCase().includes(searchTerm.toLowerCase())) {
+                searchData.push({ title, url });
+            }
+        } catch (error) {
+            console.error(`Error crawling ${seedUrl}:`, error);
+        }
+    }
+}
